dda0f738f3b45256b80ff4b6d3cfd5cf8c01b5be
Omolemo Tlomatsane <tycotecsa@gmail.com>
2024-08-09 09:22:38 +0200

    Made the button to send data to firebase

--- index.js
+++ index.js
@@ -2,12 +2,25 @@
 Challenge:
 Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
 */
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getDatabase,
+         ref,
+         push } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
+
+const firebaseConfig = {
+    databaseUrl: process.env.DATABASE_URL
+
+}
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app)
+const referenceInDB = ref(database, "CART")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-    
+    push(referenceInDB, inputValue)
     console.log(inputValue)
+    inputValue=""
 })
